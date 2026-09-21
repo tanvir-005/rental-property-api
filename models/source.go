254ae68573e8adcfedcfc610dc91f8e403e8e0fd
Md. Tanvir Ahmed Siddique <tanvir.24633@gmail.com>
2026-09-21 18:23:03 +0600

    Changed the structs to follow the hints

--- models/source.go
+++ models/source.go
@@ -1,52 +1,54 @@
 package models
 
-type LonLat struct {
-	Coordinates [2]float64 `json:"coordinates"`
+type Response struct {
+	ID string `json:"ID"`
+	Feed int `json:"Feed"`
+	Published bool `json:"Published"`
+	GeoInfo GeoInfo `json:"GeoInfo"`
+	Property Property `json:"Property"`
 }
 
-type PropertyNames struct {
-	PropertyName string `json:"property_name"`
-	PropertySlug string `json:"property_slug"`
+type GeoInfo struct {
+	Breadcrumbs []Breadcrumb `json:"Breadcrumbs"`
+	City string `json:"City"`
+	Country string `json:"Country"`
+	CountryCode string `json:"CountryCode"`
+	Name string `json:"Name"`
+	LocationID string `json:"LocationID"`
+	Lat float64 `json:"Lat"`
+	Lon float64 `json:"Lon"`
+	State string `json:"State"`
+	StateAbbr *string `json:"StateAbbr"`
 }
 
-type Location {
-	LocationId int `json:"location_id"`
-	Display string `json:"display"`
-	City string `json:"city"`
-	StateAbbr string `json:"state_abbr"`
-	State string `json:"state"`
-	CountryCode string `json:"country_code"`
-	Country string `json:"country"`
-	
-	LonLat LonLat `json:"lonlat"`
-}
-
-type PropertyDetails struct {
-	BedroomCount int `json:"bedroom_count"`
-	BathroomCount int `json:"bathroom_count"`
-}
-
-type Reviews struct {
-	NumberOfReview int `json:"number_of_review"`
-	ReviewScoreGeneral float32 `json:"review_score_general"`
-	StarRating int `json:"star_rating"`
+type Breadcrumb struct {
+	LocationID string `json:"LocationID"`
+	Name string `json:"Name"`
+	Type string `json:"Type"`
+	Slug string `json:"Slug"`
+	Display []string `json:"Display"`
 }
 
 type Property struct {
-	Id string `json:"id"`
+	Amenities []string `json:"Amenities"`
+	Name string `json:"Name"`
+	Slug string `json:"Slug"`
+	PropertyType string `json:"PropertyType"`
+	Price float64 `json:"Price"`
+	ReviewScore float64 `json:"ReviewScore"`
+	StarRating int `json:"StarRating"`
+	Counts Counts `json:"Counts"`
+	Image Image `json:"Image"`
+}
 
-	PropertyNames PropertyNames
-	Location Location
-	PropertyDetails PropertyDetails
-	Reviews Reviews
-	
-	Feed int `json:"feed"`
-	PropertyTypeCategory string `json:"property_type_category"`
-	UsdPrice float32 `json:"usd_price"`
-	Occupancy int `json:"occupancy"`
+type Counts struct {
+	Bathroom int `json:"Bathroom"`
+	Bedroom int `json:"Bedroom"`
+	Reviews int `json:"Reviews"`
+	Occupancy int `json:"Occupancy"`
+}
 
-	Images []string `json:"images"`
-	AmenityCategories []string `json:"amenity_categories"`
-	Categories string `json:"categories"`
-	Published bool `json:"published"`
+type Image struct {
+	Count int `json:"Count"`
+	Images []string `json:"Images"`
 }
